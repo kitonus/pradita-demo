70d8e08cda79460afd31fc20d812f264b48ead87
Hanendyo Wicaksono <kitonus@gamil.com>
2019-11-11 17:40:05 +0700

    Add hello page

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -1,15 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class MainController {
 
 	@GetMapping
-	public String hello() {
-		return "Hello!";
+	public RedirectView redirectToHello() {
+		return new RedirectView("/rest/greeting");
 	}
 }
