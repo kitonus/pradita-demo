e951dc0e3fb1edfaa0a3c1ef1fb20ec92798ec59
Hanendyo Wicaksono <kitonus@gamil.com>
2019-11-12 12:14:11 +0700

    Add customer list

---
+++ demo/src/main/java/com/example/demo/controller/CustomerPageController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer/page")
+public class CustomerPageController{
+	
+	@Autowired
+	private CustomerService service;
+
+	@GetMapping
+	public String list(@RequestParam(name="name", defaultValue="", required=false) String name,
+			Model model) {
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Direction.ASC, "name", "email"));
+		
+		model.addAttribute("list", service.getCustomerList(pageRequest).getCustomers().getContent());
+		return "customer/customer_list";
+	}
+}
